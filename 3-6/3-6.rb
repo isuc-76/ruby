83757edb78623c0cb440c69ee6e6a5ffd02cfb6e
isuc-76/76 <eriasimoyase@gmail.com>
2020-01-22 19:22:02 +0900

    [start] 3-6

---
+++ 3-6/3-6.rb
@@ -0,0 +1,18 @@
+puts 100
+# 出力結果=>100
+puts 100+5
+# 出力結果=>105
+
+puts "rubyを学習"
+name = "鈴木さん"
+puts "hello" + name
+puts "こんにちわ#{name}"
+
+names = ["Perl","Ruby","Python","Swift"]
+puts names[1]
+
+tall = {"太郎"=>185,"次郎"=>170,"花子"=>150}
+puts tall["太郎"]
+
+tall = {:太郎=>185, :次郎=>170, :花子=>150}
+puts tall[:花子]
